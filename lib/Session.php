eaaba7a0d1a35c027a1f1d37405d1c5201c7ccaa
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-05-01 02:06:16 +0600

    Session class created

---
+++ lib/Session.php
@@ -0,0 +1,41 @@
+<?php
+/* Session Class*/
+
+class Session{
+	
+	public static function init(){
+		session_start();
+	}
+
+	public static function set($key,$value){
+		$_SESSION['$key'] = $value;
+	}
+
+	public static function get($key){
+		
+		if (isset($_SESSION['$key'])) {
+			return $_SESSION['$key'];
+		}
+		else{
+			return false;
+		}
+	}
+
+	public static function checkSession(){
+		
+		self::init();
+		
+		if (self::get() == false) {
+			self::destroy();
+			header("location: login.php");
+		}
+	}
+
+	public static function destroy(){
+		session_destroy();
+		header("location: login.php");
+	}
+	
+}
+
+?>
