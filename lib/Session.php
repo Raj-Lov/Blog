64da63a773c376b9df4cf25111bd836da2487e2f
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-05-03 23:26:49 +0600

    simple changes in Session class

--- lib/Session.php
+++ lib/Session.php
@@ -8,13 +8,13 @@ class Session{
 	}
 
 	public static function set($key,$value){
-		$_SESSION['$key'] = $value;
+		$_SESSION[$key] = $value;
 	}
 
 	public static function get($key){
 		
-		if (isset($_SESSION['$key'])) {
-			return $_SESSION['$key'];
+		if (isset($_SESSION[$key])) {
+			return $_SESSION[$key];
 		}
 		else{
 			return false;
