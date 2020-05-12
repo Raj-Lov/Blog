1843165a18d6ca19d41ed3248bdf07d50537ffb0
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-05-12 23:53:41 +0600

    Site title and logo shown in front page

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -51,7 +51,7 @@
     <!-- Logo -->
     <a href="index.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>LTE</b></span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>Blog Dashboard</b></span>
     </a>
--- inc/header.php
+++ inc/header.php
@@ -32,13 +32,30 @@
     <div class="main_menu">
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container box_1620">
+          <?php
+
+            $query = "SELECT * FROM tbl_site_titles WHERE id = '1' ";
+            $get_data = $dbObj->select($query);
+            if ($get_data) {
+                $result = $get_data->fetch_assoc();
+
+          ?>
+
           <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+          <a class="navbar-brand logo_h" href="index.php"><img src="admin/<?php echo $result['logo'];?>" alt="Logo Here"></a>
+          <a href="index.php"><span class="title"><?php echo $result['title'] ;?> </span></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+
+          <?php
+            
+            }
+
+          ?>
+
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav justify-content-center">
