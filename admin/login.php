eaca5fe9276956e1e3dd88f94ebe2f7ef7c60c65
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-09-07 14:21:03 +0600

    Forget password system added with php mail function

--- admin/login.php
+++ admin/login.php
@@ -47,7 +47,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=""><b>Blog Login</b></a>
+    <a href=""><b>Dashboard Login</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
@@ -59,7 +59,7 @@
         $username = $dbObj->link->real_escape_string($username); 
         $password = $dbObj->link->real_escape_string($password); 
 
-        // check data exit or not
+        // check data exist or not
         $query = "SELECT * FROM tbl_users WHERE username = '$username' AND password = '$password' ";
         $result = $dbObj->select($query);
 
@@ -87,11 +87,11 @@
 
     <form action="login.php" method="post">
       <div class="form-group has-feedback">
-        <input type="username" name="username" class="form-control" placeholder="Username">
+        <input type="username" required="" name="username" class="form-control" placeholder="Username">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="Password">
+        <input type="password" required="" name="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
@@ -110,17 +110,7 @@
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <a href="forgot-password.php">Forget password? </a><br>
 
   </div>
   <!-- /.login-box-body -->
