4bd0e72d8f92b3592c8ce6b413bc69c5257952f7
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-10-07 22:00:36 +0600

    Admin panel dashboard data shown - monthly and daily post, and total statistics

--- admin/index.php
+++ admin/index.php
@@ -30,14 +30,20 @@
                 <!-- small box -->
                 <div class="small-box bg-aqua">
                     <div class="inner">
-                        <h3>290</h3>
-
+                      <?php
+                        $query = "SELECT * FROM tbl_posts" ; 
+                        $get_data = $dbObj->select($query);
+                        if ($total_row = $get_data->num_rows) {
+                              
+                      ?>
+                        <h3><?php echo $total_row; ?></h3>
+                        <?php } ?>
                         <p>Total Posts</p>
                     </div>
                     <div class="icon">
                         <i class="fa fa-credit-card"></i>
                     </div>
-                    <a href="http://localhost/employee_information_system/employee" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="viewpost.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -45,14 +51,21 @@
                 <!-- small box -->
                 <div class="small-box bg-green">
                     <div class="inner">
-                        <h3>26</h3>
+                      <?php
+                        $query = "SELECT * FROM tbl_categories" ; 
+                        $get_data = $dbObj->select($query);
+                        if ($total_row = $get_data->num_rows) {
+                              
+                      ?>
+                        <h3><?php echo $total_row; ?></h3>
+                        <?php } ?>
 
                         <p>Total Category</p>
                     </div>
                     <div class="icon">
                         <i class="fa fa-pencil"></i>
                     </div>
-                    <a href="http://localhost/employee_information_system/designation" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="viewcategory.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -60,14 +73,21 @@
                 <!-- small box -->
                 <div class="small-box bg-yellow">
                     <div class="inner">
-                        <h3>6</h3>
+                        <?php
+                        $query = "SELECT * FROM tbl_users" ; 
+                        $get_data = $dbObj->select($query);
+                        if ($total_row = $get_data->num_rows) {
+                              
+                      ?>
+                        <h3><?php echo $total_row-1; ?></h3>
+                        <?php } ?>
 
                         <p>Total Author/Editor</p>
                     </div>
                     <div class="icon">
                         <i class="fa fa-user"></i>
                     </div>
-                    <a href="http://localhost/employee_information_system/card-payment" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="viewusers.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -76,7 +96,32 @@
                 <div class="small-box bg-red">
                     <div class="inner">
 
-                        <h3>0</h3>
+                    <!-- How many post created in monthly wise count -->
+                      <?php 
+                        $currentMonth = date('m');
+
+                        $query = "SELECT date FROM tbl_posts" ; 
+                        $get_data = $dbObj->select($query);
+
+                        if ($get_data) {     
+
+                          $total_posts = 0;
+                          $i = 0;
+
+                          while ($result = $get_data->fetch_assoc()) {
+                          
+                          $getMonth = explode('-', $result['date']);
+
+                          if($getMonth[1] == $currentMonth){ 
+                            $total_posts = ++$i;
+                            
+                              }
+                            }
+                          ?>
+                            
+                        <h3><?php echo $total_posts; ?></h3>
+
+                       <?php } ?>
 
                         <p>Total Posts This Month</p>
                     </div>
@@ -101,20 +146,74 @@
                         <hr>
                     </div>
                     <!-- /.box-header -->
-                    <div class="box-body">
-                        <div id="employeeDataTable_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="employeeDataTable_length"><label>Show <select name="employeeDataTable_length" aria-controls="employeeDataTable" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="employeeDataTable_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="employeeDataTable"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="employeeDataTable" class="table table-bordered table-striped dataTable no-footer" role="grid" aria-describedby="employeeDataTable_info">
-                            <thead>
-                                <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 119.7px;" aria-sort="ascending" aria-label="SL: activate to sort column descending">SL</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 181.633px;" aria-label="Name: activate to sort column ascending">Name</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 141.5px;" aria-label="NID: activate to sort column ascending">NID</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 228.65px;" aria-label="Office ID: activate to sort column ascending">Office ID</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 191.783px;" aria-label="Phone: activate to sort column ascending">Phone</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 338.183px;" aria-label="Profile Picture: activate to sort column ascending">Profile Picture</th><th class="sorting" tabindex="0" aria-controls="employeeDataTable" rowspan="1" colspan="1" style="width: 164.55px;" aria-label="Time: activate to sort column ascending">Time</th></tr>
-                            </thead>
-                            <tbody>
 
-                            
-                            
-                            <tr class="odd"><td colspan="7" class="dataTables_empty" valign="top">No data available in table</td></tr></tbody>
+                      <div class="box-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                          <thead>
+                              <tr>
+                                  <th>No.</th>
+                                  <th>Post Title</th>
+                                  <th>Description</th>
+                                  <th>Category</th>
+                                  <th>Image</th>
+                                  <th>Author</th>
+                                  <th>Tags</th>
+                                  <th>Date</th>
+                                  <th>Action</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                          <?php
+                            $query = "SELECT tbl_posts.*, tbl_categories.name
+                                  FROM tbl_posts
+                                  INNER JOIN tbl_categories
+                                  ON tbl_posts.cat_id = tbl_categories.id
+                                  ORDER BY id DESC" ; 
+                              $posts_details = $dbObj->select($query);
+                              if ($posts_details) {
+                                  $i = 0;
+                                  while ($result = $posts_details->fetch_assoc()) {
+                                      $i++;
 
-                        </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="employeeDataTable_info" role="status" aria-live="polite">Showing 0 to 0 of 0 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="employeeDataTable_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="employeeDataTable_previous"><a href="#" aria-controls="employeeDataTable" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button next disabled" id="employeeDataTable_next"><a href="#" aria-controls="employeeDataTable" data-dt-idx="1" tabindex="0">Next</a></li></ul></div></div></div></div>
-                    </div> <br><br>
-                    <!-- /.box-body -->
+                              // Today date
+                              $todayDate = date('Y-m-d');
+
+                              // Get date from database
+                              $get_date = $result['date'];
+                              $only_date = explode(" ", $get_date);
+
+                              if($todayDate == $only_date[0]){
+
+                          ?>
+
+                              <tr>
+                                  <td><?php echo $i;?></td>
+                                  <td><?php echo $result['title'];?></td>
+                                  <td><?php echo $formatObj->postBodyShorten($result['body'],100);?></td>
+                                  <td><?php echo $result['name'];?></td>
+                                  <td><img width="50" height="50" src="<?php echo $result['image'];?>" /></td>
+                                  <td><?php echo $result['author'];?></td>
+                                  <td><?php echo $result['tags'];?></td>
+                                  <td><?php echo $formatObj->dateFormat($result['date']);?></td>
+
+                                  <td>
+                                      <a href="postview.php?post_id=<?php echo $result['id']?>">View</a>                                    
+                              </tr>
+
+                              <?php
+                              
+                                  }
+                                }
+
+                              }
+
+                              ?>
+
+                            </tbody>
+                          </table>
+                        </div>
+                  <!-- /.box-body -->
+
                 </div>
             </div>
         </div>
